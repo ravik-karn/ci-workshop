126595295f5e30da9bd92683d05cd227a03a43d2
Ravi-Kumar <ravik@thoughtworks.com>
2017-07-08 17:39:22 +0530

    Move aws configure to script

--- app_version.sh
+++ app_version.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+aws configure set aws_access_key_id $AWS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set default.region $ACCESS_KEY_REGION
+
 aws s3 cp $TRAVIS_BUILD_DIR/fulljar/democi-$TRAVIS_BUILD_NUMBER-SNAPSHOT.jar s3://aws.ravi.hellospringboot/democi-$TRAVIS_BUILD_NUMBER-SNAPSHOT.jar
 
 aws elasticbeanstalk create-application-version --application-name HelloSpringBoot \
