a64c1fde73bcb2d88f5aa549473964edac968fc5
Ravi-Kumar <ravik@thoughtworks.com>
2017-07-08 17:01:55 +0530

    add build number

--- app_version.sh
+++ app_version.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+aws s3 cp -r $TRAVIS_BUILD_DIR/fulljar s3://aws.ravi.hellospringboot
+
 aws elasticbeanstalk create-application-version --application-name HelloSpringBoot \
     --version-label SpringBootApp-v3.$TRAVIS_BUILD_NUMBER \
-    --source-bundle S3Bucket="aws.ravi.hellospringboot",S3Key="democi-0.0.1-SNAPSHOT.jar"
+    --source-bundle S3Bucket="aws.ravi.hellospringboot",S3Key="democi-"${TRAVIS_BUILD_NUMBER}"-SNAPSHOT.jar"
 
 aws elasticbeanstalk update-environment --application-name HelloSpringBoot \
     --environment-name springboot-env --version-label HelloSpringBootApp-v3.$TRAVIS_BUILD_NUMBER
-
-echo $TRAVIS_BUILD_NUMBER
-echo $TRAVIS_BUILD_DIR
