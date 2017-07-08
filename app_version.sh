6f36517662d402ee78609736aca8c722125b7516
Ravi-Kumar <ravik@thoughtworks.com>
2017-07-08 16:48:21 +0530

    add app deploy

--- app_version.sh
+++ app_version.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-aws elasticbeanstalk create-application-version --application-name SpringBoot \
+aws elasticbeanstalk create-application-version --application-name HelloSpringBoot \
     --version-label SpringBootApp-v3.$TRAVIS_BUILD_NUMBER \
     --source-bundle S3Bucket="aws.ravi.hellospringboot",S3Key="democi-0.0.1-SNAPSHOT.jar"
+
+aws elasticbeanstalk update-environment --application-name HelloSpringBoot \
+    --environment-name springboot-env --version-label HelloSpringBootApp-v3.$TRAVIS_BUILD_NUMBER
