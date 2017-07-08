b0731be32afd637862c6cf1ec41473cdb189422b
Ravi-Kumar <ravik@thoughtworks.com>
2017-07-08 16:56:37 +0530

    echo vars

--- app_version.sh
+++ app_version.sh
@@ -6,3 +6,6 @@ aws elasticbeanstalk create-application-version --application-name HelloSpringBo
 
 aws elasticbeanstalk update-environment --application-name HelloSpringBoot \
     --environment-name springboot-env --version-label HelloSpringBootApp-v3.$TRAVIS_BUILD_NUMBER
+
+echo $TRAVIS_BUILD_NUMBER
+echo $TRAVIS_BUILD_DIR
